fea60026fe44ff4412c195ac8dcb763fc63cab5c
Martin Danielsson <martin@danielsson.de>
2018-07-24 20:03:13 +0200

    Push docs to gh-pages

--- make-docs.sh
+++ make-docs.sh
@@ -11,3 +11,20 @@ fi
 
 rm -rf docs/*
 typedoc --mode modules --out ./docs ./src
+
+if [ ! -d ./docs-git ]; then
+    mkdir -p docs-git
+fi
+if [ ! -d ./docs-git/wicked.node-sdk ]; then
+    pushd docs-git
+    git clone https://github.com/apim-haufe-io/wicked.node-sdk
+    popd
+fi
+
+pushd docs-git/wicked.node-sdk
+git checkout gh-pages
+cp -R ../../docs/* .
+git add .
+git commit -m "Updated documentation."
+git push
+popd
